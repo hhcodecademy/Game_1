1737857c65d8e0d20eff10502d278a7dac74bdb3
Huseyn Hasanli <huseyn.hasanli@code.edu.az>
2024-01-27 19:22:09 +0400

    Add project files.

---
+++ Game_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Game_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random= new Random();
+            int randomNumber=random.Next(1, 100);
+            int attempts = 0;
+
+            while (true)
+            {
+                attempts++;
+                Console.WriteLine("Please enter your guess number(1-100):");
+                int myNumber=int.Parse(Console.ReadLine());
+
+                if (myNumber == randomNumber)
+                {
+                    Console.WriteLine("Congrats! You win!");
+                    Console.WriteLine($"Your attempt count: {attempts}");
+                    break;
+                }
+                else if( myNumber< randomNumber )
+                {
+                    Console.WriteLine("Your guess is too low");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is too high");
+                }
+            }
+        }
+    }
+}
